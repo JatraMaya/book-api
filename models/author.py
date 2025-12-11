ff20089beed4b4d10efbdbd33a562a3ebc21ec7c
JatraMaya <ahmad.f.iksan@gmail.com>
2025-12-11 11:43:51 +0800

    Add unique constraint for author name

--- models/author.py
+++ models/author.py
@@ -5,4 +5,5 @@ from schemas.author import AuthorBase
 
 class Author(AuthorBase, table=True):
     id: uuid.UUID | None = Field(default_factory=uuid.uuid4, primary_key=True)
+    name: str = Field(unique=True, nullable=False)
     books: list["Book"] = Relationship(back_populates="author")
