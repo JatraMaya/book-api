ca5635b226c1c6e7f217f618485da46da75b1d97
JatraMaya <ahmad.f.iksan@gmail.com>
2025-12-11 11:45:26 +0800

    Change delete response into 204 no content

--- routes/author.py
+++ routes/author.py
@@ -1,6 +1,6 @@
 from typing import Sequence
 from uuid import UUID
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 
 from schemas.author import AuthorCreate, AuthorRequest, AuthorUpdate
 from core.depends import admin_required, get_author_service
@@ -42,7 +42,31 @@ def update_author(
     return service.update_author(author_id, data)
 
 
-@admin_router.delete("/(author_id}")
+@admin_router.delete(
+    "/{author_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+    responses={
+        204: {"description": "Successfully deleted Author from system"},
+        401: {
+            "description": "Unauthorized - Authentication required",
+            "content": {
+                "application/json": {"example": {"detail": "Not authenticated"}}
+            },
+        },
+        403: {
+            "description": "Forbidden - Required Admin access",
+            "content": {
+                "application/json": {"example": {"detail": "User Unauthorized"}}
+            },
+        },
+        404: {
+            "description": "Non existing record",
+            "content": {
+                "application/json": {"example": {"detail": "Author not found"}}
+            },
+        },
+    },
+)
 def delete_author(
     author_id: UUID,
     service: AuthorService = Depends(get_author_service),
